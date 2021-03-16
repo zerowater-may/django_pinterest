75b8de73c3be173e1f4b3889890cf866c1ef1eae
ZEROWATER <kys980531@gmail.com>
2021-03-16 20:36:25 +0900

    django course 16

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 
 def hello_world(request):  
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+    
+        return render(request, 'accountapp/hello_world.html', context={'text':'post method'})
+    else:
+
+        return render(request, 'accountapp/hello_world.html', context={'text':'get method'})
