263b41f0f700bfba7a4527c31dfe5fc0058fa20d
ZEROWATER <kys980531@gmail.com>
2021-03-23 19:31:27 +0900

    djago course 24 detail views

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView  , LogoutView
-from accountapp.views import hello_world , AccountCreateView
+from accountapp.views import hello_world , AccountCreateView , AccountDetailView
 
 app_name = "accountapp"
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
 
-
     path('create/',AccountCreateView.as_view(),name='create'), # 클래스뷰는 .as_view() 를써야함 
+    path('detail/<int:pk>',AccountDetailView.as_view(),name='detail'), # 클래스뷰는 .as_view() 를써야함 
 
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse , HttpResponseRedirect
 # Create your views here.
 from django.urls import reverse , reverse_lazy
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView
+from django.views.generic import CreateView ,DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -28,3 +28,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world') #클래스에서는 reverse_lazy를 씀 . 함수에서는 reverse
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
