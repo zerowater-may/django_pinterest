2fe58e1018ad002388bbf67eef5a184eb8e2cb02
ZEROWATER <kys980531@gmail.com>
2021-03-25 17:45:45 +0900

    django course 33 / profile success

--- profileapp/decorators.py
+++ profileapp/decorators.py
@@ -1,12 +1,12 @@
-
 from django.http import HttpResponseForbidden
+
 from .models import Profile
 
-# 클래스안에서 ㅇ로그인 인증 
+
 def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        profile = Profile.objects.get(pk=kwargs['pk'])
-        if not profile.user == request.user:
+        profile = Profile.objects.get(pk=kwargs['pk']) 
+        if not profile.user == request.user: 
             return HttpResponseForbidden()
-        return func(request, *args , **kwargs)
+        return func(request, *args, **kwargs)
     return decorated
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 # Create your views here.
 from .forms import ProfileCreationForm
 from .models import Profile
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView ,UpdateView 
 from .decorators import profile_ownership_required
+from django.utils.decorators import method_decorator
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
     
     def form_valid(self, form): # ProfileCreationForm의 data가 2번째 파라미터에 들어 있어요.
@@ -20,6 +21,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()             
         return super().form_valid(form)
         
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
@@ -29,3 +33,5 @@ class ProfileUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
