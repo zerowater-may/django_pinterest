609eca38c2e1f4ff2e0addee7b6bc8da1f8a9197
ZEROWATER <kys980531@gmail.com>
2021-03-26 16:00:41 +0900

    django course 36 / listview last

--- articleapp/views.py
+++ articleapp/views.py
@@ -3,7 +3,7 @@ from django.utils.decorators import method_decorator
 from .forms import ArticleCreationForm
 from .models import Article
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView,DetailView,UpdateView,DeleteView
+from django.views.generic import CreateView,DetailView,UpdateView,DeleteView,ListView
 # Create your views here.
 from .decorators import article_ownership_required
 # @method_decorator(article_ownership_required, 'get')
@@ -47,3 +47,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
