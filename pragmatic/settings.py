a168eece8bb2c5af5bb5daf9585d9942ce547eaa
ZEROWATER <kys980531@gmail.com>
2021-04-05 15:17:00 +0900

    refresh2

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -45,12 +45,12 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.cache(),
-    # read os.environ['REDIS_URL']
-    'redis': env.cache('REDIS_URL')
-}
+# CACHES = {
+#     # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
+#     'default': env.cache(),
+#     # read os.environ['REDIS_URL']
+#     'redis': env.cache('REDIS_URL')
+# }
 
 
 
